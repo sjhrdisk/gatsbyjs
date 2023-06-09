d9fa3694fe1a449622d265ab0a8397dc43d1af6b
lm github sjhrg <114473886+sjhrdisk@users.noreply.github.com>
2023-06-09 17:53:26 +0900

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -97,83 +97,80 @@ const IndexPage = () => (
       </h1>
 	<p>튜토리얼 ↗<br />웹 개발을 처음 접하는 경우 시작하기에 좋은 곳입니다. 첫 번째 Gatsby 사이트를 설정하는 과정을 안내하도록 설계되었습니다.</p>
 
-
 <ul>
 <li><a href="http://d-cigar.com">폰테크</a></li>
 <li><a href="http://d-cigar.com/2-4">폰테크</a></li>
-<li><a href="http://d-cigar.com/?mod=document&pageid=1&uid=26">폰테크</a></li>
+<li><a href="http://d-cigar.com/8286">폰테크</a></li>
 </ul>
 
+
 <ul>
-<li><a href="https://www.facebook.com/phonetechnow">폰테크</a></li>
-<li><a href="https://smart-phone-technology-samsung-galaxy-iphone-mobile.d-cigar.com" rel="friend" title="폰테크" alt="폰테크">폰테크</a></li>
-<li><a href="https://phone-smart.org" rel="met colleague kin muse sweetheart" title="폰테크" alt="폰테크" target="_top">폰테크</a></li>
+<li><a href="https://cafe.naver.com/1djr152">폰테크</a></li>
+<li><a href="https://techmobile.kr">폰테크</a></li>
+<li><a href="https://phonelol.net">폰테크</a></li>
+<li><a href="https://phonelols.com">폰테크</a></li>
 </ul>
-		
+
+
 <ul>
-<li><a href="https://cafe.naver.com/kjhserver">폰테크</a></li>
-<li><a href="http://d-cigar.com">비대면 폰테크</a></li>
-<li><a href="http://smart-phone-technology-galaxy-iphone.d-cigar.com">내구제</a></li>
-<li><a href="http://ptkd.d-cigar.com">비대면 폰테크</a></li>
+<li><a href="http://smart-phone-technology-galaxy-iphone.d-cigar.com">폰대출</a></li>
 <li><a href="http://ptkg.d-cigar.com">부산폰테크</a></li>
-<li><a href="http://ptn1.d-cigar.com">서울폰테크</a></li>
-<li><a href="http://axp365.d-cigar.com">인천폰테크</a></li>
-<li><a href="http://fx365.d-cigar.com">광주폰테크</a></li>
-<li><a href="http://ptks.d-cigar.com">울산폰테크</a></li>
 </ul>
+
 	
 	
 	
 	<p>예시 ↗<br />Gatsby 사이트 내에서 특정 작업을 수행하는 방법을 보여주는 매우 기본적인 것부터 복잡하고 완전한 것까지 다양한 웹사이트 모음입니다.</p>
 
 <ul>
-
 <li><a href="http://livemedia-soft.com/19309?ckattempt=1">네이버 상위노출</a></li>
-
 <li><a href="http://naver.livemedia-soft.com">네이버 플레이스 상위노출</a></li>
-
-<li><a href="https://story.kakao.com/ch/livemedia/2AqGkvNCK29">웹사이트 상위노출</a></li>
-
-<li><a href="http://naver.livemedia-soft.com/">사이트 상위노출</a></li>
-
-<li><a href="http://livemedia-soft.com/338420">구글 상위노출</a></li>
-
+<li><a href="http://seo.livemedia-soft.com">네이버 검색등록</a></li>
 <li><a href="http://hp.livemedia-soft.com">네이버 사이트 등록</a></li>
+<li><a href="https://rankups.tistory.com">네이버 검색 노출</a></li>
+<li><a href="http://cafe.d-cigar.com">웹사이트 상위노출</a></li>
+</ul>
 
+<ul>
+<li><a href="https://story.kakao.com/ch/livemedia">네이버 상위노출</a></li>
+<li><a href="http://livemedia-soft.com/19309?ckattempt=1">네이버사이트순위</a></li>
+<li><a href="http://naver.livemedia-soft.com/">사이트 상위노출</a></li>
+<li><a href="http://naver.livemedia-soft.com/">네이버 사이트 순위</a></li>
+<li><a href="http://naver.livemedia-soft.com/">네이버 지도순위</a></li>
+<li><a href="http://naver.livemedia-soft.com/">상위노출</a></li>
+<li><a href="http://naver.livemedia-soft.com/">검색순위</a></li>
+</ul>
+
+
+<ul>
+<li><a href="http://livemedia-soft.com/">서버관리</a></li>
+<li><a href="http://livemedia-soft.com/rtms">서버모니터링</a></li>
+<li><a href="http://wp.livemedia-soft.com">워드프레스 홈페이지</a></li>
+<li><a href="http://page.livemedia-soft.com">반응형 홈페이지</a></li>
 <li><a href="http://link.livemedia-soft.com">백링크</a></li>
-
 </ul>
 	
 	
 	<p>플러그인 라이브러리 ↗<br />놀라운 개발자 커뮤니티에서 구축한 수천 개의 플러그인으로 기능을 추가하고 Gatsby 사이트 또는 앱을 사용자 지정하는 방법을 알아보세요.</p>
 
 	
-	
 <ul>
-<li><a href="http://di.d-cigar.com">두류스타힐스</a></li>
-<li><a href="http://xi-central-city.d-cigar.com">음성 우미린 풀하우스</a></li>
-<li><a href="http://art.d-cigar.com">원주 롯데캐슬 시그니처</a></li>
-<li><a href="http://everpark-sj.d-cigar.com">대명자이 그랜드시티</a></li>
-<li><a href="http://gang.d-cigar.com">힐스테이트 동대구 센트럴</a></li>
-
-<li><a href="http://livemedia-soft.com/">서버관리</a></li>
-<li><a href="http://livemedia-soft.com/rtms">서버모니터링</a></li>
-
-<li><a href="http://wp.livemedia-soft.com">워드프레스홈페이지</a></li>
-<li><a href="http://page.livemedia-soft.com">반응형홈페이지</a></li>
-
 <li><a href="https://panchokmul.net">판촉물</a></li>
+<li><a href="https://bitget.d-cigar.com">비트겟</a></li>
+<li><a href="https://bitget88.co.kr">바이비트</a></li>
 
-<li><a href="https://educational-topclass-study-campus.d-cigar.com">재수기숙학원</a></li>
+<li><a href="http://gang.d-cigar.com">힐스테이트 동대구 센트럴</a></li>
+<li><a href="http://everpark-sj.d-cigar.com">대명자이 그랜드시티</a></li>
+<li><a href="http://art.d-cigar.com">원주 롯데캐슬 시그니처</a></li>
+
+<li><a href="http://xi-central-city.d-cigar.com">광양 펠리시아</a></li>
+<li><a href="http://di.d-cigar.com">빌리브 헤리티지</a></li>
+
 
 <li><a href="http://quick-ticket-cultureland-pin24.d-cigar.com">문화상품권 현금화</a></li>
+<li><a href="https://educational-topclass-study-campus.d-cigar.com">재수기숙학원</a></li>
 
-<li><a href="https://bitget88.co.kr">비트겟</a></li>
-<li><a href="https://bitget.d-cigar.com">비트겟</a></li>
-
-<li><a href="http://tattoo.d-cigar.com">대구타투</a></li>
-<li><a href="http://ulsan.gogokea.com">울산중고차</a></li>
-
+<li><a href="http://camping.d-cigar.com">팝콘티비 연동</a></li>
 </ul>
 	
 	
